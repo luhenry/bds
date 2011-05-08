5660e7ff99aaa9cf9577537e3f1b75aef3a7a697
Valentin <valentin@ubuntu.ubuntu-domain>
2011-05-08 21:50:10 +0200

    Finish suBugTracker module

---
+++ apps/backend/modules/suBugTracker/lib/suBugTrackerGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * suBugTracker module configuration.
+ *
+ * @package    BDS
+ * @subpackage suBugTracker
+ * @author     Ludovic Henry
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class suBugTrackerGeneratorConfiguration extends BaseSuBugTrackerGeneratorConfiguration
+{
+}
---
+++ apps/backend/modules/suBugTracker/lib/suBugTrackerGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * suBugTracker module helper.
+ *
+ * @package    BDS
+ * @subpackage suBugTracker
+ * @author     Ludovic Henry
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class suBugTrackerGeneratorHelper extends BaseSuBugTrackerGeneratorHelper
+{
+}
